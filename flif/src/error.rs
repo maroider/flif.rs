3f357fd0f4d0d7fd03f8109904c56afabec9103a
Markus Røyset <maroider@protonmail.com>
2020-02-17 14:21:30 +0100

    Run cargo fix to update trait object and inclusive range syntax

--- flif/src/error.rs
+++ flif/src/error.rs
@@ -41,7 +41,7 @@ impl error::Error for Error {
         }
     }
 
-    fn cause(&self) -> Option<&error::Error> {
+    fn cause(&self) -> Option<&dyn error::Error> {
         match *self {
             Error::InvalidHeader { .. } => None,
             Error::Io(ref err) => Some(err),
